d900ab0a9a703ed21b112b31d5b4a23c275e0980
relliv <epsilonelittr@gmail.com>
2024-01-06 00:42:01 +0300

    Add file preview component

---
+++ src/shared/components/file-manager/filePreview.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function FileTree(props: any) {
+  const [file, setFile] = useState<any>(null);
+
+  useEffect(() => {
+    setFile(props.file);
+  }, [props.file]);
+
+  return <div className="flex flex-row">{file}</div>;
+}
